d04f1531217112f55588561ae2cca2a3093062f5
vipusrihar <vipusrihar@gmail.com>
2025-03-08 20:37:49 +0530

    Simple Login Page

---
+++ LoginPage/app/src/main/java/vipu/srihar/loginpage/MainActivity.kt
@@ -0,0 +1,30 @@
+package vipu.srihar.loginpage
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.BlendMode.Companion.Color
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import vipu.srihar.loginpage.ui.theme.LoginPageTheme
+import vipu.srihar.loginpage.ui.theme.LoginScreen
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LoginScreen()
+        }
+    }
+}
---
+++ LoginPage/app/src/main/java/vipu/srihar/loginpage/ui/theme/LoginPage.kt
@@ -0,0 +1,102 @@
+package vipu.srihar.loginpage.ui.theme
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.BlendMode.Companion.Color
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextIndent
+import androidx.compose.ui.unit.dp
+import vipu.srihar.loginpage.R
+
+
+@Composable
+fun LoginScreen(){
+
+    var username: String by remember { mutableStateOf("")}
+
+    var password: String by remember { mutableStateOf("")}
+
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Image(
+            painter =  painterResource(id = R.drawable.login),
+            contentDescription = "Image of Login",
+            modifier = Modifier.size(100.dp))
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Text(text = "E-Library")
+
+        Spacer(modifier = Modifier.height(40.dp))
+
+
+        OutlinedTextField(
+            value = username,
+            onValueChange = { username = it} ,
+            label = {
+            Text(text = "UserName")
+        })
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = {password = it},
+            visualTransformation = PasswordVisualTransformation(),
+            label = {
+            Text(text = "Password")
+        })
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Button(colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF000000)), onClick = { }){
+            Text(text = "Login",color = Color(0xFFFFFFFF))
+        }
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Text(text = "Forgot Password", modifier = Modifier.clickable{})
+
+        Spacer(modifier = Modifier.height(40.dp))
+
+        Text(text = "SignUp With")
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+
+        Image(
+            painter = painterResource(id=R.drawable.google),
+            contentDescription = "google",
+            modifier = Modifier.size(50.dp)
+        )
+
+
+
+
+    }
+
+}
